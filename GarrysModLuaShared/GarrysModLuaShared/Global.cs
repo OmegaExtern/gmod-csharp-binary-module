b8ecf5c7d419e4051435c3106fcf470bf485df62
OmegaExtern <omegaextern@live.com>
2015-10-22 04:32:01 +0200

    Added require (Global) function.

--- GarrysModLuaShared/GarrysModLuaShared/Global.cs
+++ GarrysModLuaShared/GarrysModLuaShared/Global.cs
@@ -19,5 +19,15 @@ namespace GarrysModLuaShared
                 lua_pcall(luaState, args.Length, 0, 0);
             }
         }
+
+        public static void require(IntPtr luaState, string name)
+        {
+            lock (SyncRoot)
+            {
+                lua_getfield(luaState, (int)TableIndex.SpecialGlob, nameof(require));
+                lua_pushstring(luaState, name);
+                lua_pcall(luaState, 1, 0, 0);
+            }
+        }
     }
 }
