6dc11edd56a8ca463c7c4ff82e2dce46ddb4ead4
OmegaExtern <omegaextern@live.com>
2015-10-22 03:02:34 +0200

    Registered IsDir function (hopefully it should work fine).

--- GarrysModLuaShared/GarrysModLuaShared/DllMain.cs
+++ GarrysModLuaShared/GarrysModLuaShared/DllMain.cs
@@ -11,6 +11,7 @@ namespace GarrysModLuaShared
         public static int gmod13_open(IntPtr luaState)
         {
             RegisterCFunction(luaState, nameof(ExternalFile).ToLowerInvariant(), nameof(ExternalFile.Delete), ExternalFile.Delete);
+            RegisterCFunction(luaState, nameof(ExternalFile).ToLowerInvariant(), nameof(ExternalFile.IsDir), ExternalFile.IsDir);
             RegisterCFunction(luaState, nameof(ExternalFile).ToLowerInvariant(), nameof(ExternalFile.Read), ExternalFile.Read);
             RegisterCFunction(luaState, nameof(ExternalFile).ToLowerInvariant(), nameof(ExternalFile.Write), ExternalFile.Write);
             return 0;
