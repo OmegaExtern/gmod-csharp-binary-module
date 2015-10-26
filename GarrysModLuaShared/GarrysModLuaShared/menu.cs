a9629f4079da0472a432ad6ac24b1eacf66289bb
OmegaExtern <omegaextern@live.com>
2015-10-26 21:07:27 +0100

    Implemented menu library.

---
+++ GarrysModLuaShared/GarrysModLuaShared/menu.cs
@@ -0,0 +1,23 @@
+﻿#if CLIENT
+using System;
+using static GarrysModLuaShared.Lua;
+
+namespace GarrysModLuaShared
+{
+    /// <summary>Used to interface with the built in game events system.</summary>
+    static class menu
+    {
+        /// <summary>Used by "Demo to Video" to record the frame.</summary>
+        /// <param name="luaState">Pointer to lua_State struct.</param>
+        public static void RecordFrame(IntPtr luaState)
+        {
+            lock (SyncRoot)
+            {
+                lua_getglobal(luaState, nameof(menu));
+                lua_getfield(luaState, -1, nameof(RecordFrame));
+                lua_pcall(luaState, 1);
+            }
+        }
+    }
+}
+#endif
