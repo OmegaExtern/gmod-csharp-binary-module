cf0caa171a4675fc59d4da5693800cbbf6bf4d3d
OmegaExtern <omegaextern@live.com>
2015-10-26 13:05:30 +0100

    Implemented gameevent library.

---
+++ GarrysModLuaShared/GarrysModLuaShared/gameevent.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using static GarrysModLuaShared.Lua;
+
+namespace GarrysModLuaShared
+{
+    /// <summary>Used to interface with the built in game events system.</summary>
+    static class gameevent
+    {
+        /// <summary>Add a game event listener.</summary>
+        /// <param name="luaState">Pointer to lua_State struct.</param>
+        /// <param name="eventName">The event to listen to, travels through hooks with eventName as event.<para/>List of valid events can be found <see cref="http://wiki.garrysmod.com/page/Game_Events"/>.</param>
+        public static void Listen(IntPtr luaState, string eventName)
+        {
+            lock (SyncRoot)
+            {
+                lua_getglobal(luaState, nameof(gameevent));
+                lua_getfield(luaState, -1, nameof(Listen));
+                lua_pushstring(luaState, eventName);
+                lua_pcall(luaState);
+            }
+        }
+    }
+}
